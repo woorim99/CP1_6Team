1a6a76878aba366f14bdd1da8044fb6e962a8664
mooncw <97713997+mooncw@users.noreply.github.com>
2022-07-06 15:34:32 +0900

    Update views.py

--- dancesite/dancecheck/views.py
+++ dancesite/dancecheck/views.py
@@ -23,7 +23,7 @@ def members(request):
 def display(request, image_id):
 
 
-    model2000 = load_model('dancecheck/model16000')
+    model32000 = load_model('dancecheck/model32000')
     input = './dancecheck/static/dancecheck/image/'+ image_id + '_F_00003140.jpg' 
 
     X_test=[]
@@ -32,11 +32,12 @@ def display(request, image_id):
         img=tf.io.read_file(test_path)
         img=tf.image.decode_jpeg(img, channels=3)
         img=tf.image.resize(img, [180,320])
+        img=img/255
         X_test.append(img)
 
     X_test=tf.stack(X_test, axis=0)
 
-    pred=model2000.predict(X_test)
+    pred=mode32000.predict(X_test)
 
     pred = pred[0]
 
@@ -48,29 +49,29 @@ def display(request, image_id):
     pose = np.zeros((1080, 1920, 3), np.uint8)
 
     for j in range(0,len(pred),2):
-        cv2.line(keypoint, (int(pred[j]*8), int(pred[j+1]*8)), (int(pred[j]*8), int(pred[j+1]*8)), color, 10)
-        cv2.line(pose, (int(pred[j]*8), int(pred[j+1]*8)), (int(pred[j]*8), int(pred[j+1]*8)), color, 30)
+        cv2.line(keypoint, (int(pred[j]*5.4), int(pred[j+1]*5.4)), (int(pred[j]*5.4), int(pred[j+1]*5.4)), color, 10)
+        cv2.line(pose, (int(pred[j]*5.4), int(pred[j+1]*5.4)), (int(pred[j]*5.4), int(pred[j+1]*5.4)), color, 30)
     #코부터 엉덩이 R
-    cv2.line(pose, (int(pred[48]*8), int(pred[49]*8)), (int(pred[26]*8), int(pred[27]*8)), red, 10)
-    cv2.line(pose, (int(pred[26]*8), int(pred[27]*8)), (int(pred[24]*8), int(pred[25]*8)), red, 10)
-    cv2.line(pose, (int(pred[24]*8), int(pred[25]*8)), (int(pred[22]*8), int(pred[23]*8)), red, 10)
-    cv2.line(pose, (int(pred[22]*8), int(pred[23]*8)), (int(pred[0]*8), int(pred[1]*8)), red, 10)
+    cv2.line(pose, (int(pred[48]*5.4), int(pred[49]*5.4)), (int(pred[26]*5.4), int(pred[27]*5.4)), red, 10)
+    cv2.line(pose, (int(pred[26]*5.4), int(pred[27]*5.4)), (int(pred[24]*5.4), int(pred[25]*5.4)), red, 10)
+    cv2.line(pose, (int(pred[24]*5.4), int(pred[25]*5.4)), (int(pred[22]*5.4), int(pred[23]*5.4)), red, 10)
+    cv2.line(pose, (int(pred[22]*5.4), int(pred[23]*5.4)), (int(pred[0]*5.4), int(pred[1]*5.4)), red, 10)
     #오른 어깨부터 오른손 G
-    cv2.line(pose, (int(pred[38]*8), int(pred[39]*8)), (int(pred[40]*8), int(pred[41]*8)), green, 10)
-    cv2.line(pose, (int(pred[40]*8), int(pred[41]*8)), (int(pred[42]*8), int(pred[43]*8)), green, 10)
-    cv2.line(pose, (int(pred[42]*8), int(pred[43]*8)), (int(pred[44]*8), int(pred[45]*8)), green, 10)
-    cv2.line(pose, (int(pred[42]*8), int(pred[43]*8)), (int(pred[46]*8), int(pred[47]*8)), green, 10)
+    cv2.line(pose, (int(pred[38]*5.4), int(pred[39]*5.4)), (int(pred[40]*5.4), int(pred[41]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[40]*5.4), int(pred[41]*5.4)), (int(pred[42]*5.4), int(pred[43]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[42]*5.4), int(pred[43]*5.4)), (int(pred[44]*5.4), int(pred[45]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[42]*5.4), int(pred[43]*5.4)), (int(pred[46]*5.4), int(pred[47]*5.4)), green, 10)
     #왼 어깨부터 왼손 G
-    cv2.line(pose, (int(pred[28]*8), int(pred[29]*8)), (int(pred[30]*8), int(pred[31]*8)), green, 10)
-    cv2.line(pose, (int(pred[30]*8), int(pred[31]*8)), (int(pred[32]*8), int(pred[33]*8)), green, 10)
-    cv2.line(pose, (int(pred[32]*8), int(pred[33]*8)), (int(pred[34]*8), int(pred[35]*8)), green, 10)
-    cv2.line(pose, (int(pred[32]*8), int(pred[33]*8)), (int(pred[36]*8), int(pred[37]*8)), green, 10)
+    cv2.line(pose, (int(pred[28]*5.4), int(pred[29]*5.4)), (int(pred[30]*5.4), int(pred[31]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[30]*5.4), int(pred[31]*5.4)), (int(pred[32]*5.4), int(pred[33]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[32]*5.4), int(pred[33]*5.4)), (int(pred[34]*5.4), int(pred[35]*5.4)), green, 10)
+    cv2.line(pose, (int(pred[32]*5.4), int(pred[33]*5.4)), (int(pred[36]*5.4), int(pred[37]*5.4)), green, 10)
     #오른쪽 엉덩이 부터 오른 발목 B
-    cv2.line(pose, (int(pred[12]*8), int(pred[13]*8)), (int(pred[14]*8), int(pred[15]*8)), blue, 10)
-    cv2.line(pose, (int(pred[14]*8), int(pred[15]*8)), (int(pred[16]*8), int(pred[17]*8)), blue, 10)
+    cv2.line(pose, (int(pred[12]*5.4), int(pred[13]*5.4)), (int(pred[14]*5.4), int(pred[15]*5.4)), blue, 10)
+    cv2.line(pose, (int(pred[14]*5.4), int(pred[15]*5.4)), (int(pred[16]*5.4), int(pred[17]*5.4)), blue, 10)
     #왼쪽 엉덩이 부터 왼쪽 발목 B
-    cv2.line(pose, (int(pred[2]*8), int(pred[3]*8)), (int(pred[4]*8), int(pred[5]*8)), blue, 10)
-    cv2.line(pose, (int(pred[4]*8), int(pred[5]*8)), (int(pred[6]*8), int(pred[7]*8)), blue, 10)
+    cv2.line(pose, (int(pred[2]*5.4), int(pred[3]*5.4)), (int(pred[4]*5.4), int(pred[5]*5.4)), blue, 10)
+    cv2.line(pose, (int(pred[4]*5.4), int(pred[5]*5.4)), (int(pred[6]*5.4), int(pred[7]*5.4)), blue, 10)
 
     background = Image.open(input)
     foreground1 = Image.fromarray(keypoint).convert("1")
